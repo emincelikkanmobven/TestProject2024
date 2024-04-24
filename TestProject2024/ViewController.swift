135ad2acf9d0866a7442088a148f2cf8091c5b86
emincelikkanmobven <emin.celikkan@mobven.com>
2024-04-24 20:14:00 +0300

    Initial Commit

---
+++ TestProject2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject2024
+//
+//  Created by Emin Çelikkan on 24.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
