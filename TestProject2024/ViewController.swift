9443908766484e20523c5f4a69bf7b8a0c6022d1
emincelikkanmobven <emin.celikkan@mobven.com>
2024-04-25 12:56:25 +0300

    Created doSomething func
    doSomething func prints user status on logs.

--- TestProject2024/ViewController.swift
+++ TestProject2024/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    private func doSomething() {
+        print("user status on log")
+    }
 }
 
